d8431b42396061c997f7ec8bc182111af800d06a
AxelHowe <75995668+AxelHowe@users.noreply.github.com>
2021-05-30 22:24:03 +0800

    three

--- populate/production.py
+++ populate/production.py
@@ -2,5 +2,5 @@ from populate import base
 from user.models import User
 
 print('Creating admin account ... ', end='')
-User.objects.create_superuser(username='admin', password='87z632xx87', email=None, fullName='管理者')
+User.objects.create_superuser(username='admin', password='87z632xx87', email=None,)
 print('done')
